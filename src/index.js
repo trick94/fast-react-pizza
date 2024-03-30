864a2ec8e66f1bf94056b4493c7cc8a3e8d89788
trick94 <patrick.pepito94@gmail.com>
2024-03-31 00:38:14 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function App() {
 function Header() {
   return (
     <header className="header">
-      <h1>Fast React Pizza Co.</h1>;
+      <h1>Fast React Pizza Co.</h1>
     </header>
   );
 }
